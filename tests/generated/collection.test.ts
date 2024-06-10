0307b83eb88681e924b31a5f7c07f78f6e05728e
Yuchan Lee <lasuillard@gmail.com>
2024-06-10 12:46:00 +0000

    test: Test `collection.getChildCollections()`

--- tests/generated/collection.test.ts
+++ tests/generated/collection.test.ts
@@ -89,7 +89,86 @@ it("removeCollections", async ({ client, expect, generateTypeTest }) => {
 	`);
 });
 
-it.todo("getChildCollections");
+it("getChildCollections", async ({
+	client,
+	expect,
+	generateTypeTest,
+	resetData: _,
+}) => {
+	const parent = await client.collection.createCollection({
+		view: "list",
+		title: "createCollection",
+		sort: 0,
+		public: true,
+		parent: {
+			$ref: "collections",
+			$id: 0,
+			oid: 0,
+		},
+		cover: [],
+	});
+	await client.collection.createCollection({
+		view: "list",
+		title: "nestedCollection",
+		sort: 0,
+		public: true,
+		parent: {
+			$ref: "collections",
+			// biome-ignore lint/style/noNonNullAssertion: PASS
+			$id: parent.data.item!._id,
+			oid: 0,
+		},
+		cover: [],
+	});
+
+	const response = await client.collection.getChildCollections();
+	generateTypeTest({
+		type: "CollectionResponseMany",
+	});
+	expect(response.data).toMatchInlineSnapshot(`
+		{
+		  "items": [
+		    {
+		      "_id": 44998269,
+		      "access": {
+		        "draggable": true,
+		        "for": 2067190,
+		        "level": 4,
+		        "root": false,
+		      },
+		      "author": true,
+		      "count": 0,
+		      "cover": [],
+		      "created": "2024-06-10T12:28:58.050Z",
+		      "creatorRef": {
+		        "_id": 2067190,
+		        "email": "",
+		        "name": "miyil99106",
+		      },
+		      "description": "",
+		      "expanded": true,
+		      "lastAction": "2024-06-10T12:28:58.050Z",
+		      "lastUpdate": "2024-06-10T12:28:58.050Z",
+		      "parent": {
+		        "$id": 44998268,
+		        "$ref": "collections",
+		      },
+		      "public": true,
+		      "slug": "nested-collection",
+		      "sort": 0,
+		      "title": "nestedCollection",
+		      "user": {
+		        "$id": 2067190,
+		        "$ref": "users",
+		      },
+		      "view": "list",
+		    },
+		  ],
+		  "result": true,
+		}
+	`);
+});
+
 it.todo("getCollection");
 it.todo("updateCollection");
 it.todo("removeCollection");
