67fac61066901739e324e8199d3d5618141a1ed5
Yuchan Lee <lasuillard@gmail.com>
2024-06-10 12:53:40 +0000

    test: Apply rate limit for Axios test client

--- tests/_helpers/axios.ts
+++ tests/_helpers/axios.ts
@@ -2,6 +2,7 @@ import type { Use } from "@vitest/runner";
 import { Configuration } from "^/src/generated";
 import axios, { type AxiosInstance } from "axios";
 import MockAdapter from "axios-mock-adapter";
+import rateLimit from "axios-rate-limit";
 import { Raindrop } from "~/client";
 
 // biome-ignore lint/correctness/noEmptyPattern: PASS
@@ -9,7 +10,8 @@ export async function axiosInstance({}, use: Use<AxiosInstance>) {
 	const instance = axios.create({
 		validateStatus: () => true,
 	});
-	await use(instance);
+	const rateLimited = rateLimit(instance, { maxRPS: 3 });
+	await use(rateLimited);
 }
 
 export async function mockAxios(
