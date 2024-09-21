c82f96e9a4854447fd179ec8bf47096fef0e3b21
Yuchan Lee <lasuillard@gmail.com>
2024-09-21 22:27:18 +0900

    chore(devcontainer): Update `onCreateCommand.sh`
    Signed-off-by: Yuchan Lee <lasuillard@gmail.com>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-sudo apt-get update && sudo apt-get install -y bash-completion
-
-echo '
-if [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-fi
-' >>~/.bashrc
+sudo apt-get update && sudo apt-get install -y \
+    bash-completion
